709354548303140caf7282659425f27a3317569b
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-20 18:11:19 +0900

    htmlの記述

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <form className="mb-4 space-y-3">
+      <input type='text' className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-400" />
+      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform transition-transform duration-200 hover:bg-blue-400 hover:scale-95">
+        AddTask</button>
+      </form>
   )
 }
 
--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
-  )
+  <ul className="space-y-3">
+    <li className="flex justify-between p-4 bg-white border-blue-500 rounded shadow">
+      <span>散歩</span>
+      <div>
+        <button className="text-green-500 mr-3">edit</button>
+        <button className="text-red-500">delete</button>
+      </div>
+    </li>
+    <li className="flex justify-between p-4 bg-white border-blue-500 rounded shadow">
+      <span>散歩</span>
+      <div>
+        <button className="text-green-500 mr-3">edit</button>
+        <button className="text-red-500">delete</button>
+      </div>
+    </li>    <li className="flex justify-between p-4 bg-white border-blue-500 rounded shadow">
+      <span>散歩</span>
+      <div>
+        <button className="text-green-500 mr-3">edit</button>
+        <button className="text-red-500">delete</button>
+      </div>
+    </li>
+  </ul>
+)
 }
 
 export default TodoList
